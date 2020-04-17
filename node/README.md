f6287b8a93d502e73e8a747d1230551a4b681e99
Miguel Tomé <aluno19993@ipt.pt>
2020-04-17 15:08:42 +0100

    start of the node project
    start of the node project

---
+++ node/README.md
@@ -0,0 +1,2 @@
+# Ti2_Client_Side_render_Final
+Trabalho final da cadeira de Ti2 da parte de Client Side Render
---
+++ node/src/services/recipe-service.js
@@ -0,0 +1,34 @@
+exports.getRecipes=()=>{
+    return new Promise((resolve,reject)=>{
+
+    });
+
+};
+
+exports.getRecipe= id =>{
+    return new Promise((resolve,reject)=>{
+        
+    });
+
+};
+
+exports.insert= body =>{
+    return new Promise((resolve,reject)=>{
+        
+    });
+
+};
+
+exports.update=(id, body)=>{
+    return new Promise((resolve,reject)=>{
+        
+    });
+
+};
+
+exports.remove=id=>{
+    return new Promise((resolve,reject)=>{
+        
+    });
+
+};
